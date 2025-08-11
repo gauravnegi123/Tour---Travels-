e3c55031985ce2663866943790ec2a56b50c7218
gauravnegi123 <gauravnegi6783@gmail.com>
2025-08-11 23:14:16 +0530

    css update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const signin = document.getElementById("signin");
+const close = document.getElementById("close");
+const loginpopup = document.getElementById("loginpopup");
 
 
+
+
+
+
+signin.addEventListener("click", () => {
+    loginpopup.style.display = 'flex';
+});
+
+close.addEventListener("click", () => {
+    loginpopup.style.display = 'none';
+});
 
